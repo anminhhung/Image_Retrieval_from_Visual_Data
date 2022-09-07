87159c8db21ecdf3b2702ac038c6dfdea6096d40
tuanngoc01 <tuanngoc06102000@gmail.com>
2022-09-07 14:11:56 +0700

    Change get_transforms function of dataset.py and remove config.py

--- data_loader/dataset.py
+++ data_loader/dataset.py
@@ -39,11 +39,11 @@ class LandmarkDataset(Dataset):
 def get_transforms(image_size):
 
     transforms_train = albumentations.Compose([
-        albumentations.HorizontalFlip(p=0.5),
-        albumentations.ImageCompression(quality_lower=99, quality_upper=100),
-        albumentations.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=10, border_mode=0, p=0.7),
+        # albumentations.HorizontalFlip(p=0.5),
+        # albumentations.ImageCompression(quality_lower=99, quality_upper=100),
+        # albumentations.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=10, border_mode=0, p=0.7),
         albumentations.Resize(image_size, image_size),
-        albumentations.Cutout(max_h_size=int(image_size * 0.4), max_w_size=int(image_size * 0.4), num_holes=1, p=0.5),
+        # albumentations.Cutout(max_h_size=int(image_size * 0.4), max_w_size=int(image_size * 0.4), num_holes=1, p=0.5),
         albumentations.Normalize()
     ])
 
